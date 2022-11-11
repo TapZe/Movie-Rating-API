337bded1ea853800f0e7feaad7117bebb13c87b2
TapZe <nabilmuyassar@Gmail.com>
2022-11-11 22:55:25 +0800

    Database Update

--- Backend/Movie API Database.sql
+++ Backend/Movie API Database.sql
@@ -16,6 +16,36 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_movie_rating` /*!40100 DEFAULT CHARA
 
 USE `db_movie_rating`;
 
+/*Table structure for table `rating_list` */
+
+DROP TABLE IF EXISTS `rating_list`;
+
+CREATE TABLE `rating_list` (
+  `movie_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` enum('1','2','3','4','5') NOT NULL,
+  `comment` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`movie_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `rating_list` */
+
+/*Table structure for table `user_list` */
+
+DROP TABLE IF EXISTS `user_list`;
+
+CREATE TABLE `user_list` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_type` enum('ADMIN','MEMBER') NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user_list` */
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
