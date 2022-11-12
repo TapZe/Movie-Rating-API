d4b9838aae83b79ce995048ddfb1c56594450455
TapZe <nabilmuyassar@Gmail.com>
2022-11-12 09:27:25 +0800

    Add Full Login and Register API

--- Backend/BackendAPI.py
+++ Backend/BackendAPI.py
@@ -1,8 +1,19 @@
 from flask import Flask, request, jsonify, make_response
-import pymysql, requests, hashlib
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required, JWTManager
+import pymysql
+import datetime
+import hashlib
+import requests
 
-# Membuat server flask
+# Membuat server flask dan API KEY TMDB
 app = Flask(__name__)
+app_key = "aef829310f8af509d6ebabe33b18f3e9"
+
+# Flask JWT Extended Configuration
+app.config['SECRET_KEY'] 					= "INI_SECRET_KEY"
+app.config['JWT_HEADER_TYPE']				= "JWT"
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] 		= datetime.timedelta(days=1) #1 hari token JWT expired
+jwt = JWTManager(app)
 
 mydb = pymysql.connect(
 	host="localhost",
@@ -23,7 +34,6 @@ def get_trend_movie_list(time):
 		url = ""
 		payload={}
 		headers = {}
-		app_key = "aef829310f8af509d6ebabe33b18f3e9"
 
 		if time == "day":
 			url = "https://api.themoviedb.org/3/trending/movie/day?api_key={}".format(app_key)
@@ -33,7 +43,7 @@ def get_trend_movie_list(time):
 		response = requests.request("GET", url, headers=headers, data=payload)
 
 		if int(response.status_code) != 200:
-			raise Exception("Status Code tidak 200")
+			raise Exception("Status Code is not 200")
 
 		response_json = response.json()
 
@@ -41,15 +51,14 @@ def get_trend_movie_list(time):
 	
 	except Exception as e:
 		response_json = {
-			"status" : "Gagal mendapatkan list movie",
-			"description" : str(e)
+			"Status" : "Failed to get trending movie list",
+			"Description" : str(e)
 		}
 		return make_response(jsonify(response_json), 200)
 
 @app.route('/get_popular_movie_list/<page>', methods=['GET'])
 def get_popular_movie_list(page):
 	try:
-		app_key = "aef829310f8af509d6ebabe33b18f3e9"
 		url = "https://api.themoviedb.org/3/movie/popular?api_key={}".format(app_key)
 		payload= {}
 		headers = {}
@@ -60,7 +69,7 @@ def get_popular_movie_list(page):
 		response = requests.request("GET", url, headers=headers, data=payload)
 
 		if int(response.status_code) != 200:
-			raise Exception("Status Code tidak 200")
+			raise Exception("Status Code is not 200")
 
 		response_json = response.json()
 
@@ -68,8 +77,8 @@ def get_popular_movie_list(page):
 	
 	except Exception as e:
 		response_json = {
-			"status" : "Gagal mendapatkan list movie",
-			"description" : str(e)
+			"Status" : "Failed to get popular movie list",
+			"Description" : str(e)
 		}
 		return make_response(jsonify(response_json), 200)
 
@@ -77,27 +86,93 @@ def get_popular_movie_list(page):
 ### USER API ###
 @app.route('/register_user', methods=['POST'])
 def register_user():
-	hasil = {"status": "gagal register data user"}
+	hasil = {"Status": "Register failed"}
 
 	try:
 		data = request.json
 		username = data["username"]
 		password = data["password"]
 
-		username = username.lower()
-		password_enc = hashlib.md5(password.encode('utf-8')).hexdigest() # Convert password to md5
+		if len(password) < 8:
+			hasil = {"Status": "Your password needs to be atleast 8 character!"}
+			return jsonify(hasil)
 
+		username = username.lower()
+		password_enc = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+		# Cek apakah username ada didalam database
+		query = " SELECT username FROM user_list WHERE username = %s "
+		values = (username, )
+
+		mycursor = mydb.cursor()
+		mycursor.execute(query, values)
+		data_user = mycursor.fetchall()
+
+		if len(data_user) > 0:
+			hasil = {"Status": "The username that you enter already exsisted!"}
+			return jsonify(hasil)
+
+		# Register data baru
 		query = "INSERT INTO user_list (username, password, user_type) VALUES(%s,%s,%s)"
 		values = (username, password_enc, "MEMBER",)
 		mycursor = mydb.cursor()
 		mycursor.execute(query, values)
 		mydb.commit()
-		hasil = {"status": "berhasil register data user"}
+		hasil = {"Status": "Register successful"}
 
 	except Exception as e:
 		print("Error: " + str(e))
 
 	return jsonify(hasil)
+
+@app.route("/login_user", methods=["POST"])
+def login():
+	access_token = ""
+
+	try:
+		data = request.json
+
+		username = data["username"]
+		password = data["password"]
+
+		username = username.lower()
+		password_enc = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+		# Cek kredensial didalam database
+		query = " SELECT user_id, password, user_type FROM user_list WHERE username = %s "
+		values = (username, )
+
+		mycursor = mydb.cursor()
+		mycursor.execute(query, values)
+		data_user = mycursor.fetchall()
+
+		if len(data_user) == 0:
+			hasil = {"Status": "The username that you enter does not exsist!"}
+			return jsonify(hasil)
+
+		data_user	= data_user[0]
+
+		user_id 	= data_user[0]
+		db_password = data_user[1]
+		user_type	= data_user[2]
+
+		if password_enc != db_password:
+			hasil = {"Status": "The password you enter is wrong!"}
+			return jsonify(hasil)
+
+		jwt_payload = {
+			"id_user" : user_id,
+			"role" : user_type
+		}
+
+		access_token = create_access_token(username, additional_claims=jwt_payload)
+
+	except Exception as e:
+		print("Error: " + str(e))
+
+	return jsonify(access_token=access_token)
+
+
 
 ### RATING API ###
 
